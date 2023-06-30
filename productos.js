c192febb64e4a73de4cabd4ae1cacd3e0b42e93b
Map4che <ljuan648l@gmail.com>
2023-06-29 23:15:00 -0500

    Terminando index con medidas

--- productos.js
+++ productos.js
@@ -1,2 +1,352 @@
-let productosNuevos = document.getElementById("productos-nuevos-img");
+let images = [
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'kenworth',
+        product: 'motor'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'nuestras-ofertas',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'nuestras-ofertas',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1674272975/Map4che_dcyhvg.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-buscado',
+        brand: 'kenworth'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683949470/facebook-logotype_iljfnj.png',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'nuestras-ofertas',
+        brand: 'international'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'productos-nuevos',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-buscado',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683945174/v982-d4-01_zsifhq.jpg',
+        title: 'Lorem ipsum',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-buscado',
+        brand: 'chevrolet'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683944911/v982-d4-01_ksim6m.jpg',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-buscado',
+        brand: 'ford',
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683944911/v982-d4-01_ksim6m.jpg',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'ford'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683944911/v982-d4-01_ksim6m.jpg',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'ford',
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683944911/v982-d4-01_ksim6m.jpg',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'ford'
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683944911/v982-d4-01_ksim6m.jpg',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'ford',
+    },
+    {
+        src:'https://res.cloudinary.com/dmiocpap8/image/upload/v1683944911/v982-d4-01_ksim6m.jpg',
+        description:'accusantium quas sint, nemo inventore id. Esse!',
+        category:'mas-vendido',
+        brand: 'ford',
+    }
+]
 
+
+let categoria;
+let categoriaNum =0;
+let marca =0;
+let producto =0;
+let productosMostrar = document.getElementById("elementos__elegidos");
+
+function definirCategoria(categoriaNum){
+switch (categoriaNum){
+    case 0: categoria = ""
+    return categoria
+    break;
+    case 1: categoria ="productos-nuevos";
+    return categoria
+    break;
+    case 2: categoria ="nuestras-ofertas";
+    return categoria
+    break;
+    case 3: categoria ="mas-vendido";
+    return categoria
+    break;
+    case 4: categoria ="mas-buscado";
+    return categoria
+    break;
+}
+}
+
+for (let i = 0; i < images.length; i++) {
+  let image = images[i];
+  let imageElement = document.createElement("img");
+  imageElement.src = image.src;
+
+  let titleElement = document.createElement("h2");
+  titleElement.textContent = image.title;
+
+  let descriptionElement = document.createElement("p");
+  descriptionElement.textContent = image.description;
+
+  let imageContainer = document.createElement("div");
+  imageContainer.appendChild(imageElement);
+  imageContainer.appendChild(titleElement);
+  imageContainer.appendChild(descriptionElement);
+
+  productosMostrar.appendChild(imageContainer);
+}
+
+function mostrarElementosPorCategoriaYMarca(categoria = '', marca = '') {
+  productosMostrar.innerHTML = ''; // Limpia el contenedor antes de mostrar los elementos filtrados
+
+  for (let i = 0; i < images.length; i++) {
+    let image = images[i];
+
+    // Comprueba si la categoría y la marca del objeto coinciden con las seleccionadas (ignora el filtro si el parámetro está vacío)
+    if ((categoria === '' || image.category === categoria) && (marca === '' || image.brand === marca)) {
+      let imageElement = document.createElement("img");
+      imageElement.src = image.src;
+
+      let titleElement = document.createElement("h2");
+      titleElement.textContent = image.title;
+
+      let descriptionElement = document.createElement("p");
+      descriptionElement.textContent = image.description;
+
+      let imageContainer = document.createElement("div");
+      imageContainer.appendChild(imageElement);
+      imageContainer.appendChild(titleElement);
+      imageContainer.appendChild(descriptionElement);
+
+      productosMostrar.appendChild(imageContainer);
+    }
+  }
+}
+
+let botonProductosNuevos = document.getElementById("botonProductosNuevos");
+let botonNuestrasOfertas = document.getElementById("botonNuestrasOfertas");
+let botonMasVendidos = document.getElementById("botonMasVendidos");
+let botonMasBuscados = document.getElementById("botonMasBuscados");
+
+let botonMarcaKenworth = document.getElementById("botonMarcaKenworth");
+let botonMarcaInternational = document.getElementById("botonMarcaInternational");
+let botonMarcaChevrolet = document.getElementById("botonMarcaChevrolet");
+let botonMarcaFord = document.getElementById("botonMarcaFord");
+
+botonProductosNuevos.addEventListener("click", function() {
+  mostrarElementosPorCategoriaYMarca("productos-nuevos", "");
+  categoriaNum=1;
+});
+
+botonNuestrasOfertas.addEventListener("click", function() {
+    mostrarElementosPorCategoriaYMarca("nuestras-ofertas", "");
+    categoriaNum = 2;
+  });
+
+botonMasVendidos.addEventListener("click", function() {
+    mostrarElementosPorCategoriaYMarca("mas-vendido", "");
+    categoriaNum =3;
+  });
+
+botonMasBuscados.addEventListener("click", function() {
+    mostrarElementosPorCategoriaYMarca("mas-buscado", "");
+    categoriaNum = 4;
+  });
+
+
+botonMarcaKenworth.addEventListener("click", function() {
+    categoria = definirCategoria(categoriaNum);
+  mostrarElementosPorCategoriaYMarca(categoria, "kenworth");
+});
+
+botonMarcaInternational.addEventListener("click", function() {
+    categoria = definirCategoria(categoriaNum);
+    mostrarElementosPorCategoriaYMarca(categoria, "international");
+  });
+
+  botonMarcaChevrolet.addEventListener("click", function() {
+    categoria = definirCategoria(categoriaNum);
+    mostrarElementosPorCategoriaYMarca(categoria, "chevrolet");
+  });
+
+  botonMarcaFord.addEventListener("click", function() {
+    categoria = definirCategoria(categoriaNum);
+    mostrarElementosPorCategoriaYMarca(categoria, "ford");
+  });
+
+
+
+mostrarElementosPorCategoriaYMarca(); // Muestra todos los elementos
+mostrarElementosPorCategoriaYMarca("productos-nuevos"); // Filtra por categoría "productos-nuevos"
+mostrarElementosPorCategoriaYMarca("", "kenworth"); // Filtra por marca "kenworth"
+mostrarElementosPorCategoriaYMarca("productos-nuevos", "kenworth"); // Filtra por categoría "productos-nuevos" y marca "kenworth"
